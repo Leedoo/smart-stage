ab228df5b0dd0ee133773c3ba6add926c493c9de
zhoule <zhoule@daojia-inc.com>
2024-03-15 17:25:04 +0800

    初始提交

---
+++ smart-stage-core/src/main/java/com/smart/stage/core/entity/IErrorCode.java
@@ -0,0 +1,33 @@
+package com.smart.stage.core.entity;
+
+/**
+ * 错误码基础接口
+ *
+ * @author Joe
+ */
+public interface IErrorCode extends IMessage {
+
+    /**
+     * 错误码
+     */
+    Integer getCode();
+
+    /**
+     * 错误码描述
+     */
+    default String getDesc(){
+        return getKey();
+    }
+
+    /**
+     * 错误码code用做I18n的key
+     */
+    default String getKey() {
+        return getCode().toString();
+    }
+
+    @Override
+    default String getMessage(Object... args) {
+        return Message.getOrDefault(getKey(), getDesc(), args);
+    }
+}
